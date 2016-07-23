498fd5bcf258f3a9f99fdac54c256420f7ce935a
vns <you@example.com>
2016-07-23 22:21:40 +0300

    New changelist

---
+++ j2se/concurrency/src/main/java/concurrency/ConcurrentHashMapExample.java
@@ -0,0 +1,62 @@
+package concurrency;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import org.junit.Assert;
+
+public class ConcurrentHashMapExample {
+    static class ObjectTest {
+        private int value;
+
+        @Override
+        public int hashCode() {
+            return 7;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return ((ObjectTest) obj).value == value;
+        }
+
+        public ObjectTest(int value) {
+            this.value = value;
+        }
+    }
+
+    private void testPutMethod(Map<ObjectTest, ObjectTest> hashMap) throws InterruptedException {
+        final CountDownLatch downLatch = new CountDownLatch(2);
+
+        createThread(hashMap, downLatch, 1, 1000).start();
+        createThread(hashMap, downLatch, 1001, 2000).start();
+
+        downLatch.await();
+    }
+
+    private Thread createThread(Map<ObjectTest, ObjectTest> hashMap, CountDownLatch downLatch, int start, int end) {
+        return new Thread(() -> {
+            try {
+                for (int i = start; i < end; i++) {
+                    ObjectTest obj = new ObjectTest(i);
+                    hashMap.put(obj, obj);
+                    ObjectTest obj2 = hashMap.get(obj);
+                    Assert.assertEquals(obj.value, obj2 == null ? 0 : obj2.value);
+                }
+            } catch (Throwable t) {
+                t.printStackTrace();
+            } finally {
+                downLatch.countDown();
+            }
+        });
+    }
+
+    public static void main(String... args) throws InterruptedException {
+        ConcurrentHashMap<ObjectTest, ObjectTest> concurrentHashMap = new ConcurrentHashMap<>();
+        HashMap<ObjectTest, ObjectTest> hashMap = new HashMap<>();
+        System.out.print("\nstart method with HashMap\n");
+        new ConcurrentHashMapExample().testPutMethod(hashMap);
+        System.out.print("\nstart method with ConcurrentHashMap\n");
+        new ConcurrentHashMapExample().testPutMethod(concurrentHashMap);
+    }
+}
